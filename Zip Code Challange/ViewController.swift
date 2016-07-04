6af30137320b27e13ab167380f8bfed0ef5c4dc2
Jose Virgen <j.virgen87@gmail.com>
2016-07-04 13:22:44 -0700

    Initial Commit

---
+++ Zip Code Challange/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zip Code Challange
+//
+//  Created by Jose Virgen on 7/4/16.
+//  Copyright © 2016 Jose Virgen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
